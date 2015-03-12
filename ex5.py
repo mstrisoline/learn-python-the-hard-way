092059614a58d845943fee982960e2f10d239b93
mstrisoline <mstrisoline@gmail.com>
2015-03-11 21:25:49 -0400

    Initial Commit

---
+++ ex5.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+name = "Zed A. Shaw"
+age = 35
+height = 74
+weight = 180
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+print "Let's talk about %s." % name
+print "He's %d inches tall." % height
+print "He's %d pounds heavy." % weight
+
+print 'Actually thats not too eavy.'
+print "He's got %s eys and %s hair" % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
+
+print "If I add %d, %d, and %d I get %d." % (
+  age, height, weight, age + height + weight )
